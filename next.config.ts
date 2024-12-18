1f1c2d6d244d7afcb06b2a0e8fa2c0837084d5a6
Rajesh Verma <i.rajeshv11@gmail.com>
2024-12-18 12:35:05 +0530

    Update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/",
 };
 
 export default nextConfig;
